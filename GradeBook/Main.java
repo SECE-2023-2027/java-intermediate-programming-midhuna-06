c4244605f000f6d7504bbcaf492ab7963b413aa4
midhuna-06 <midhuna.a2023cse@sece.ac.in>
2026-07-01 14:00:45 +0530

    Solved Gradebook problem

---
+++ GradeBook/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Enter Student Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Number of Subjects: ");
+        int n = sc.nextInt();
+
+        int[] marks;
+
+        if (n <= 0) {
+            marks = new int[0];
+        } else {
+            marks = new int[n];
+
+            System.out.println("Enter Marks:");
+            for (int i = 0; i < n; i++) {
+                marks[i] = sc.nextInt();
+            }
+        }
+
+        Student s = new Student(name, marks);
+
+        System.out.println(s.calculateAverage());
+
+        sc.close();
+    }
+}
---
+++ GradeBook/Student.java
@@ -0,0 +1,27 @@
+public class Student {
+
+    
+    private String name;
+    private int[] marks;
+
+    
+    public Student(String name, int[] marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+
+
+    public double calculateAverage() {
+        if (marks == null || marks.length == 0) {
+            return 0.0;
+        }
+
+        int sum = 0;
+
+        for (int mark : marks) {
+            sum += mark;
+        }
+
+        return (double) sum / marks.length;
+    }
+}
