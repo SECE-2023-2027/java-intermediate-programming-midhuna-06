46b9432146347cf6cd4e300c2a4f9ce5023c2378
midhuna-06 <midhuna.a2023cse@sece.ac.in>
2026-07-01 15:40:11 +0530

    Added Bank problem

---
+++ Bank/BankAccount.java
@@ -0,0 +1,44 @@
+package Bank;
+
+public class BankAccount {
+
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    
+    public BankAccount(String accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            System.out.println("Invalid Deposit Amount");
+        } else {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > balance) {
+            System.out.println("Insufficient Funds");
+        } else {
+            balance -= amount;
+        }
+    }
+}
---
+++ Bank/Main.java
@@ -0,0 +1,35 @@
+package Bank;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Account Number: ");
+        String accountNumber = sc.nextLine();
+
+        System.out.print("Enter Account Holder Name: ");
+        String accountHolderName = sc.nextLine();
+
+        System.out.print("Enter Initial Balance: ");
+        double balance = sc.nextDouble();
+
+        BankAccount account = new BankAccount(accountNumber, accountHolderName, balance);
+
+        System.out.print("Enter Deposit Amount: ");
+        double depositAmount = sc.nextDouble();
+        account.deposit(depositAmount);
+
+    
+        System.out.print("Enter Withdraw Amount: ");
+        double withdrawAmount = sc.nextDouble();
+        account.withdraw(withdrawAmount);
+
+       
+        System.out.println(account.getBalance());
+
+    }
+}
